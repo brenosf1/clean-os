e45aa44ee0170fe43ea7090d16d8b5fadea5dc24
Breno Farias <56539373+brenosf1@users.noreply.github.com>
2026-02-13 15:00:09 -0300

    Create post-install-remove.sh

---
+++ files/scripts/post-install-remove.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm -f /usr/local/bin/morewaita.sh
+rm -f /usr/share/icons/LICENSE
+rm -f /usr/share/icons/README.md
+rm -f /usr/share/icons/get-it-on-ge.png
+rm -f /usr/share/icons/v2.3.1.jpg
+rm -f /usr/share/icons/setup
+rm -f /usr/share/icons/morewaita.sh
+rm -f /usr/share/icons/.git
